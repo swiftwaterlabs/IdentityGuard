3ec9b2799641d35e493f450e6627c66d8ced0af4
Josh Rolstad <jrolstad@gmail.com>
2021-09-19 08:37:51 -0700

    Stubbing out cosmos service

--- IdentityGuard.Api/Configuration/DependencyInjectionConfiguration.cs
+++ IdentityGuard.Api/Configuration/DependencyInjectionConfiguration.cs
@@ -1,5 +1,12 @@
-﻿using IdentityGuard.Core.Managers;
+﻿using System;
+using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using IdentityGuard.Core.Configuration;
+using IdentityGuard.Core.Managers;
 using IdentityGuard.Core.Services;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -15,6 +22,76 @@ namespace IdentityGuard.Api.Configuration
             services.AddTransient<AuthorizationManager>();
 
             services.AddTransient<EnvironmentService>();
+
+            ConfigureKeyVault(services);
+            ConfigureCosmosDb(services);
         }
+
+        private static void ConfigureKeyVault(IServiceCollection services)
+        {
+            services.AddTransient(provider =>
+            {
+                var configuration = provider.GetService<IConfiguration>();
+                var endpoint = configuration["KeyVault:BaseUri"];
+                var endpointUrl = new Uri(endpoint);
+                var managedIdentityClientId = configuration["KeyVault:ManagedIdentityClientId"];
+
+                var credentials = GetCredential(managedIdentityClientId);
+
+                return new SecretClient(vaultUri: endpointUrl, credential: credentials);
+            });
+
+        }
+
+        private static TokenCredential GetCredential(string clientId)
+        {
+            if(string.IsNullOrWhiteSpace(clientId)) return new DefaultAzureCredential();
+
+            return new ManagedIdentityCredential(clientId);
+        }
+
+        private static void ConfigureCosmosDb(IServiceCollection services)
+        {
+            services.AddTransient(provider =>
+            {
+                var secretClient = provider.GetService<SecretClient>();
+                var configuration = provider.GetService<IConfiguration>();
+                var endpoint = configuration["Cosmos:BaseUri"];
+
+                var secret = secretClient.GetSecret("cosmos-primary-key");
+                var key = secret.Value.Value;
+
+                var client = GetCosmosClient(endpoint, key);
+
+                return client;
+            });
+
+            services.AddTransient(provider =>
+            {
+                var client = provider.GetService<CosmosClient>();
+                var queryFactory = provider.GetService<ICosmosLinqQueryFactory>();
+                return new CosmosDbService(client,CosmosConfiguration.DatabaseId,queryFactory);
+            });
+
+            services.AddTransient<ICosmosLinqQueryFactory, CosmosLinqQueryFactory>();
+        }
+
+        private static CosmosClient GetCosmosClient(string endpoint, string key)
+        {
+            var options = new CosmosClientOptions
+            {
+                AllowBulkExecution = true,
+                MaxRetryAttemptsOnRateLimitedRequests = 19,
+                MaxRetryWaitTimeOnRateLimitedRequests = TimeSpan.FromMinutes(1),
+                SerializerOptions = new CosmosSerializationOptions
+                {
+                    IgnoreNullValues = true
+                }
+            };
+
+            var client = new CosmosClient(endpoint, key, options);
+            return client;
+        }
+        
     }
 }
--- IdentityGuard.Core/Services/CosmosDbService.cs
+++ IdentityGuard.Core/Services/CosmosDbService.cs
@@ -1,7 +1,121 @@
-﻿namespace IdentityGuard.Core.Services
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+
+namespace IdentityGuard.Core.Services
 {
     public class CosmosDbService
     {
-        
+        private readonly CosmosClient _client;
+        private readonly ICosmosLinqQueryFactory _cosmosLinqQueryFactory;
+
+        public CosmosDbService(CosmosClient cosmosClient, string defaultDatabaseId, ICosmosLinqQueryFactory cosmosLinqQueryFactory)
+        {
+            _client = cosmosClient;
+            _cosmosLinqQueryFactory = cosmosLinqQueryFactory;
+        }
+
+        public IQueryable<T> Query<T>(string collectionId, string databaseId = null)
+        {
+            var result = _client.GetContainer(databaseId, collectionId)
+                .GetItemLinqQueryable<T>(allowSynchronousQueryExecution: true);
+
+            return result;
+        }
+
+        public async Task<T> Get<T>(string id, string containerId, string partitionKey, string databaseId = null)
+        {
+            try
+            {
+                var result = await _client.GetContainer(databaseId, containerId)
+                    .ReadItemAsync<T>(id, new PartitionKey(partitionKey));
+
+                return result.Resource;
+            }
+            catch (CosmosException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    return default(T);
+
+                throw;
+            }
+        }
+
+        public async Task<T> Save<T>(T toSave, string collectionId, string databaseId = null)
+        {
+            var result = await _client.GetContainer(databaseId, collectionId)
+                .UpsertItemAsync(toSave);
+
+            return result.Resource;
+        }
+
+        public async Task Save<T>(IEnumerable<T> toSave, string collectionId, Func<T, string> getPartitionKey, string databaseId = null)
+        {
+            var container = _client.GetContainer(databaseId, collectionId);
+
+            var upsertTasks = toSave
+                .Select(item => container.UpsertItemAsync(item, new PartitionKey(getPartitionKey(item))));
+
+            await Task.WhenAll(upsertTasks);
+        }
+
+        public async Task<T> Delete<T>(string id, string containerId, string partitionKey, string databaseId = null)
+        {
+            var result = await _client.GetContainer(databaseId, containerId)
+                .DeleteItemAsync<T>(id, new PartitionKey(partitionKey));
+
+            return result.Resource;
+        }
+
+        public async Task<ICollection<TOut>> ExecuteRead<TIn, TOut>(IQueryable<TIn> query, Func<TIn, TOut> mapper)
+        {
+            var iterator = _cosmosLinqQueryFactory.GetFeedIterator(query);
+
+            var result = new List<TOut>();
+            while (iterator.HasMoreResults)
+            {
+                var inputData = await iterator.ReadNextAsync();
+                var mappedData = inputData.Select(mapper);
+                result.AddRange(mappedData);
+            }
+
+            return result;
+        }
+
+        public async Task<ICollection<T>> ExecuteRead<T>(IQueryable<T> query)
+        {
+            var iterator = _cosmosLinqQueryFactory.GetFeedIterator(query);
+
+            var result = new List<T>();
+            while (iterator.HasMoreResults)
+            {
+                result.AddRange(await iterator.ReadNextAsync());
+            }
+
+            return result;
+        }
+
+    }
+
+    public interface ICosmosLinqQueryFactory
+    {
+        FeedIterator<T> GetFeedIterator<T>(IQueryable<T> query);
+    }
+
+    public class CosmosLinqQueryFactory : ICosmosLinqQueryFactory
+    {
+        /// <summary>
+        /// Wraps to FeedIterator so we have a seam that is usable for testing
+        /// </summary>
+        /// <see cref="https://github.com/Azure/azure-cosmos-dotnet-v3/issues/893"/>
+        public FeedIterator<T> GetFeedIterator<T>(IQueryable<T> query)
+        {
+            return query.ToFeedIterator();
+        }
+
+
     }
 }
