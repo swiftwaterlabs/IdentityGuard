a9d5a4d7e73de19d6e961433f236d03580cb7f30
Josh Rolstad <jorolsta@microsoft.com>
2021-09-24 16:14:38 -0700

    Adding permissions

---
+++ IdentityGuard.Blazor.Ui/Components/ApplicationPermissionCards.razor.cs
@@ -0,0 +1,15 @@
+﻿using IdentityGuard.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityGuard.Blazor.Ui.Components
+{
+    public partial class ApplicationPermissionCards
+    {
+        [Parameter]
+        public ILookup<string, ApplicationPermission> Data { get; set; }
+    }
+}
