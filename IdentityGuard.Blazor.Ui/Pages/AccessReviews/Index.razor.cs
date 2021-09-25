be843d5d559182162c32b9ab83b7da6a1afe2e5d
Josh Rolstad <jrolstad@gmail.com>
2021-09-24 19:29:12 -0700

    Stubbing out reviews

---
+++ IdentityGuard.Blazor.Ui/Pages/AccessReviews/Index.razor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using IdentityGuard.Blazor.Ui.Models;
+using IdentityGuard.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace IdentityGuard.Blazor.Ui.Pages.AccessReviews
+{
+    public partial class Index
+    {
+        [Inject]
+        public AppState AppState { get; set; }
+
+        public bool IsLoading { get; set; } = false;
+
+        public List<AccessReview> PendingAccessReviews { get; set; } = new ();
+        public List<AccessReview> CompletedAccessReviews { get; set; } = new ();
+
+        protected override Task OnInitializedAsync()
+        {
+            AppState.SetBreadcrumbs(
+                new BreadcrumbItem("Access Reviews", Paths.AccessReviews)
+            );
+
+            return base.OnParametersSetAsync();
+        }
+
+        public Task StartNewAccessReview()
+        {
+            return Task.CompletedTask;
+        }
+
+        public void ShowAccessReview(string id)
+        {
+
+        }
+
+        public void ShowNew()
+        {
+
+        }
+    }
+}
