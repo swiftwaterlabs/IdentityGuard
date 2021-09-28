94727d54fe9045024df5c64e439e96d4fe8dec21
Josh Rolstad <jorolsta@microsoft.com>
2021-09-28 09:29:43 -0700

    Adding role details for applications

---
+++ IdentityGuard.Blazor.Ui/Components/RoleCards.razor.cs
@@ -0,0 +1,14 @@
+﻿using IdentityGuard.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System.Linq;
+
+namespace IdentityGuard.Blazor.Ui.Components
+{
+    public partial class RoleCards
+    {
+        [Parameter]
+        public ILookup<string, Role> Data { get; set; }
+
+        public bool ShowDefaultAccess { get; set; }
+    }
+}
---
+++ IdentityGuard.Blazor.Ui/Components/RoleGroupCard.razor.cs
@@ -0,0 +1,18 @@
+﻿using IdentityGuard.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace IdentityGuard.Blazor.Ui.Components
+{
+    public partial class RoleGroupCard
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public IEnumerable<Role> Data { get; set; }
+
+        [Parameter]
+        public bool ShowDefaultAccess { get; set; }
+    }
+}
