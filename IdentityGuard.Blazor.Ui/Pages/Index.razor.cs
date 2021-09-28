e81e728640e931ccba8448c299ad429b389a6f2b
Josh Rolstad <jorolsta@microsoft.com>
2021-09-28 13:29:32 -0700

    Updating index page

---
+++ IdentityGuard.Blazor.Ui/Pages/Index.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityGuard.Blazor.Ui.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public void ShowAccessReviewPage()
+        {
+            NavigationManager.NavigateTo(Paths.AccessReviews);
+        }
+    }
+}
