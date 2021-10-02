b7b16f14a5ddd5dbc4892a1fa6f9c6d434a2ec8c
Josh Rolstad <jorolsta@microsoft.com>
2021-10-01 20:45:15 -0700

    Moving things to shared projects

--- IdentityGuard.Api.Tests/TestUtility/TestBuilder.cs
+++ IdentityGuard.Api.Tests/TestUtility/TestBuilder.cs
@@ -1,82 +1,26 @@
 ﻿using System.Collections.Generic;
-using Azure.Security.KeyVault.Secrets;
 using IdentityGuard.Api.Configuration;
 using IdentityGuard.Api.Functions;
-using IdentityGuard.Tests.Shared.Fakes;
-using IdentityGuard.Tests.Shared.TestContexts;
-using IdentityGuard.Core.Services;
-using Microsoft.Azure.Cosmos;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using IdentityGuard.Tests.Shared;
-using IdentityGuard.Tests.Shared.Extensions;
 
 namespace IdentityGuard.Api.Tests.TestUtility
 {
-    public class TestBuilder:ITestBuilder
+    public class TestBuilder:TestBuilderBase
     {
-        private static ServiceProvider _serviceProvider;
-
-        public TestBuilder() : this(new ServiceCollection())
-        {
-
-        }
-        private TestBuilder(IServiceCollection services)
-        {
-            Configure(services);
-        }
-
-        private static void Configure(IServiceCollection services)
-        {
-            ConfigureApplicationConfiguration(services);
-            ConfigureApplication(services);
-            ConfigureFunctions(services);
-            ConfigureFakes(services);
-
-            _serviceProvider = services.BuildServiceProvider();
-        }
-
-        private static void ConfigureApplication(IServiceCollection services)
+        protected override void ConfigureApplication(IServiceCollection services)
         {
             var properties = new Dictionary<object, object>();
             var context = new HostBuilderContext(properties);
             DependencyInjectionConfiguration.Configure(context, services);
         }
 
-        private static void ConfigureApplicationConfiguration(IServiceCollection services)
+        protected override void ConfigureFunctions(IServiceCollection services)
         {
-            var configuration = new ConfigurationBuilder()
-                .AddInMemoryCollection()
-                .Build();
-
-            services.AddSingleton<IConfiguration>(configuration);
-        }
-
-        private static void ConfigureFunctions(IServiceCollection services)
-        {
-            services.AddFunctionsWorkerCore();
-            services.AddFunctionsWorkerDefaults();
-
             services.AddTransient<AuthorizationFunction>();
             services.AddTransient<HealthFunction>();
             services.AddTransient<DirectoryFunctions>();
         }
-
-        private static void ConfigureFakes(IServiceCollection services)
-        {
-            services.AddSingleton<TestContext>();
-            services.ReplaceTransient<CosmosClient, CosmosClientFake>();
-            services.ReplaceTransient<SecretClient, SecretClientFake>();
-            services.ReplaceTransient<ICosmosLinqQueryFactory, CosmosLinqQueryFactoryFake>();
-        }
-
-        public T Get<T>()
-        {
-            return _serviceProvider.GetService<T>();
-        }
-
-        public TestContext Context => _serviceProvider.GetService<TestContext>();
-
     }
 }
--- IdentityGuard.Tests.Shared/ITestBuilder.cs
+++ IdentityGuard.Tests.Shared/ITestBuilder.cs
@@ -1,14 +1,85 @@
-﻿using IdentityGuard.Tests.Shared.TestContexts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Azure.Security.KeyVault.Secrets;
+using IdentityGuard.Tests.Shared.Fakes;
+using IdentityGuard.Tests.Shared.TestContexts;
+using IdentityGuard.Core.Services;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using IdentityGuard.Tests.Shared;
+using IdentityGuard.Tests.Shared.Extensions;
+using IdentityGuard.Core.Configuration;
 
 namespace IdentityGuard.Tests.Shared
 {
     public interface ITestBuilder
     {
         TestContext Context { get; }
+    }
+
+    public class TestBuilderBase:ITestBuilder
+    {
+        private static ServiceProvider _serviceProvider;
+
+        public TestBuilderBase() : this(new ServiceCollection())
+        {
+
+        }
+        private TestBuilderBase(IServiceCollection services)
+        {
+            Configure(services);
+        }
+
+        private void Configure(IServiceCollection services)
+        {
+            ConfigureApplicationConfiguration(services);
+            ConfigureApplication(services);
+            ConfigureFunctionsWorker(services);
+            ConfigureFunctions(services);
+            ConfigureFakes(services);
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private static void ConfigureApplicationConfiguration(IServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection()
+                .Build();
+
+            services.AddSingleton<IConfiguration>(configuration);
+        }
+
+        private static void ConfigureFunctionsWorker(IServiceCollection services)
+        {
+            services.AddFunctionsWorkerCore();
+            services.AddFunctionsWorkerDefaults();
+        }
+
+        protected virtual void ConfigureApplication(IServiceCollection services)
+        {
+
+        }
+
+        protected virtual void ConfigureFunctions(IServiceCollection services)
+        {
+            
+        }
+
+        private static void ConfigureFakes(IServiceCollection services)
+        {
+            services.AddSingleton<TestContext>();
+            services.ReplaceTransient<CosmosClient, CosmosClientFake>();
+            services.ReplaceTransient<SecretClient, SecretClientFake>();
+            services.ReplaceTransient<ICosmosLinqQueryFactory, CosmosLinqQueryFactoryFake>();
+        }
+
+        public T Get<T>()
+        {
+            return _serviceProvider.GetService<T>();
+        }
+
+        public TestContext Context => _serviceProvider.GetService<TestContext>();
     }
 }
